1b473648ca7aadd6f5a25a818669e6f1c824b46d
armandupe <symbolic28@gmail.com>
2020-02-19 16:48:25 +0300

    fix

--- script.js
+++ script.js
@@ -9,10 +9,42 @@ new Vue({
         if (text != '') {
           this.todos.push({text, done: false, id: Math.random()})
           event.target.value = ''
+
+          let delBtn = document.querySelector('.btn-del-all');
+          delBtn.style.display = 'block'
         }
+        
+      },
+      addTodoOnClick() {
+        let text = document.querySelector('.todo-app__input')
+        text = text.value
+        if (text != '') {
+          this.todos.push({text, done: false, id: Math.random()})
+          let clearInputValue = document.querySelector('.todo-app__input')
+          clearInputValue.value = ''
+
+          let delBtn = document.querySelector('.btn-del-all');
+          delBtn.style.display = 'block'
+        }
+        
       },
       removeTodo(id) {
         this.todos = this.todos.filter(todo => todo.id !== id)
+        if (this.todos.length == 0) {
+          let delBtn = document.querySelector('.btn-del-all');
+          delBtn.style.display = 'none'
+        }
+      },
+      removeAll() {
+        let li = document.querySelectorAll('.todo-app__list-element')
+        for (let i = 0; i < li.length; i++) {
+          const listEl = li[i];
+          listEl.parentNode.removeChild(listEl)
+        }
+
+        let delBtn = document.querySelector('.btn-del-all');
+        delBtn.style.display = 'none'
+        
       },
       check(todo) {
         todo.done = !todo.none
