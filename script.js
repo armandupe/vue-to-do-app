c98f5410c9ee2d07eabfcd515e62a02af7c2bdb7
armandupe <symbolic28@gmail.com>
2020-12-10 16:20:31 +0300

    somefixes

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
 new Vue({
-    el: "#app",
-    data: {
-      todos: []
-    },
-    methods: {
-      addTodo(event) {
-        const text = event.target.value
-        if (text != '') {
-          this.todos.push({text, done: false, id: Math.random()})
-          event.target.value = ''
-
-          let delBtn = document.querySelector('.btn-del-all');
-          delBtn.style.display = 'block'
-        }
-        
-      },
-      addTodoOnClick() {
-        let text = document.querySelector('.todo-app__input')
-        text = text.value
-        if (text != '') {
-          this.todos.push({text, done: false, id: Math.random()})
-          let clearInputValue = document.querySelector('.todo-app__input')
-          clearInputValue.value = ''
-
-          let delBtn = document.querySelector('.btn-del-all');
-          delBtn.style.display = 'block'
-        }
-        
-      },
-      removeTodo(id) {
-        this.todos = this.todos.filter(todo => todo.id !== id)
-        if (this.todos.length == 0) {
-          let delBtn = document.querySelector('.btn-del-all');
-          delBtn.style.display = 'none'
-        }
-      },
-      removeAll() {
-        let li = document.querySelectorAll('.todo-app__list-element')
-        for (let i = 0; i < li.length; i++) {
-          const listEl = li[i];
-          listEl.parentNode.removeChild(listEl)
-        }
+  el: "#app",
+  data: {
+    todos: []
+  },
+  methods: {
+    addTodo() {
+      let text = document.querySelector('.todo-app__input');
+      text = text.value;
+      const regExp = text.match(/\s{2,}/g);
+console.log(this.todos);
+      if (text != '' && text != ' ' && !regExp) {
+        this.todos.push({ text, done: false, id: Math.random() })
+        let clearInputValue = document.querySelector('.todo-app__input');
+        clearInputValue.value = '';
 
         let delBtn = document.querySelector('.btn-del-all');
-        delBtn.style.display = 'none'
-        
-      },
-      check(todo) {
-        todo.done = !todo.none
+        delBtn.style.display = 'block';
+
+        const errorMsg = document.querySelector('.todo-app__input-error-msg');
+        errorMsg.classList.add('hidden');
+        errorMsg.textContent = ' ';
+      } else {
+        const errorMsg = document.querySelector('.todo-app__input-error-msg');
+        errorMsg.classList.remove('hidden');
+        errorMsg.textContent = 'Напишите что-нибудь, пожалуйста :)';
       }
-     
+    },
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+      if (this.todos.length == 0) {
+        let delBtn = document.querySelector('.btn-del-all');
+        delBtn.style.display = 'none';
+      }
+    },
+    removeAll() {
+      const li = document.querySelectorAll('.todo-app__list-element')
+      for (let i = 0; i < li.length; i++) {
+        const listEl = li[i];
+        listEl.parentNode.removeChild(listEl);
+      }
+
+      let delBtn = document.querySelector('.btn-del-all');
+      delBtn.style.display = 'none';
+
+    },
+    check(todo) {
+      todo.done = !todo.none;
     }
+
+  }
 })     
