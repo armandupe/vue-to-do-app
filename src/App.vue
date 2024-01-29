82d8addb920535ac4555609a74b77fbb38cd5795
Dmitrii Kudriavtsev <symbolic28@gmail.com>
2024-01-29 16:21:23 +0300

    refactor in progress

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import ToDoList from './components/ToDoList.vue'
+</script>
+
+<template>
+  <header v-text="'Vue 3 ToDo App'" />
+  <main class="todo-app">
+    <ToDoList />
+  </main>
+  <footer>armandupe 2024</footer>
+</template>
+
+<style scoped></style>
